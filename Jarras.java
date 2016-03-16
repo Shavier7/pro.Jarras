d5aed614f527055ca096d68390fd9301a1f809e9
Shavier7 <shavierseven@gmail.com>
2016-03-16 17:33:26 +0100

    Creando el fichero Jarras.java
    Creamos el archivo Java para empezar a programarlo en las diferentes 3
    ramas.

--- Jarras.java
+++ Jarras.java
@@ -12,13 +12,7 @@ public class Jarras {
 			this.capacidad=capacidadInicial;
 		}
 	//metodos
-	//D1
-		public void llenaDesde(Jarra j){
-			while((this.cantidad<this.capacidad)&&(j.cantidad>0)){
-				this.cantidad++;
-				j.cantidad--;
-			} ;
-
+	
 		
 		
 	public void metodoComunitario(){
